b35f21cfbaaa0c3f8d322729bd0224ee5cdbe8c8
DESKTOP-JL94ITU\TiTiIsMe <tintran1197@gmail.com>
2020-09-21 16:49:10 +0700

    clean code

--- src/Components/ListScreen.js
+++ src/Components/ListScreen.js
@@ -2,7 +2,21 @@ import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
-  return <Text>List Scrddeen</Text>;
+  const friends = [
+    { name: "Friend 1" },
+    { name: "Friend 2" },
+    { name: "Friend 3" },
+    { name: "Friend 4" },
+    { name: "Friend 5" },
+  ];
+  return (
+    <FlatList
+      data={friends}
+      renderItem={({ item }) => {
+        return <Text>{item.name}</Text>;
+      }}
+    />
+  );
 };
 const styles = StyleSheet.create({});
 
