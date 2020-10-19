edd765ffa740056e56f22ae17c6514b717684fa2
DESKTOP-JL94ITU\TiTiIsMe <tintran1197@gmail.com>
2020-10-20 00:05:43 +0700

    add popup dialog after update info, add placeName in request-details

--- src/screens/profile/profile.component.jsx
+++ src/screens/profile/profile.component.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, Text } from "react-native";
 
 import AvatarNameCol from "../../components/avatar-name-column.component";
 import BackgroundImage from "../../components/background-screen.component";
@@ -11,10 +11,36 @@ import KeyboardAvoiding from "../../components/keyboard-avoiding.component";
 import styles from "./profile.styles";
 
 const ProfileScreen = ({ navigation }) => {
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
         <BackgroundImage>
             <KeyboardAvoiding style={styles.container}>
-                <HeaderTileWithBackBtn textContent="Hồ sơ bệnh" gotoScreen={() => navigation.navigate("Home")} />
+                <View
+                    style={[
+                        styles.modal,
+                        modalVisible ? { opacity: 0.85, zIndex: 10 } : null,
+                    ]}
+                >
+                    <View style={styles.modal__content}>
+                        <Text style={styles.status}>
+                            Cập nhật thông tin thành công
+                        </Text>
+                        <Text
+                            onPress={() => {
+                                setModalVisible(false);
+                                navigation.navigate("Home");
+                            }}
+                            style={styles.action}
+                        >
+                            Đóng
+                        </Text>
+                    </View>
+                </View>
+                <HeaderTileWithBackBtn
+                    textContent="Hồ sơ bệnh"
+                    gotoScreen={() => navigation.navigate("Home")}
+                />
                 <AvatarNameCol
                     imgSource={require("../../../assets/icons/mock-avatar.png")}
                     textContent="Tín Trần"
@@ -29,7 +55,7 @@ const ProfileScreen = ({ navigation }) => {
                         textContent="Cập nhật"
                         styleButton={styles.button}
                         styleText={styles.button_text}
-                        gotoScreen={() => navigation.navigate("Home")}
+                        gotoScreen={() => setModalVisible(true)}
                     />
                 </View>
             </KeyboardAvoiding>
--- src/screens/profile/profile.styles.js
+++ src/screens/profile/profile.styles.js
@@ -2,17 +2,55 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        width: "100%"
+        width: "100%",
     },
     container_button: {
         alignItems: "center",
         justifyContent: "center",
-        marginBottom: 20
+        marginBottom: 20,
     },
     button_text: {
         color: "#26324A",
-        paddingVertical: 2
-    }
+        paddingVertical: 2,
+    },
+    modal: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#000",
+        zIndex: -1,
+        opacity: 0,
+    },
+    modal__content: {
+        width: "90%",
+        height: "25%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        borderRadius: 20,
+        paddingHorizontal: 10,
+    },
+    status: {
+        fontFamily: "Texgyreadventor-bold",
+        fontSize: 18,
+        marginTop: 30,
+    },
+    action: {
+        position: "absolute",
+        paddingVertical: 8,
+        paddingHorizontal: 40,
+        borderRadius: 20,
+        backgroundColor: "#5E00FF",
+        fontFamily: "Texgyreadventor-regular",
+        fontSize: 16,
+        color: "#fff",
+        bottom: 10,
+    },
 });
 
 export default styles;
